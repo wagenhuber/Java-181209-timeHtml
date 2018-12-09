9156fd482257c59c08b7b1d26858182526cfbfa2
wagenhuber <wagenhuber@gmx.net>
2018-12-09 19:14:24 +0100

    changed to version 3

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Date date = new Date();
+    public static void main(String[] args) throws IOException, InterruptedException {
         File file = new File("index.html");
-        System.out.println(date.toString());
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write(date.toString());
-        fileWriter.write(" -> created by Version 2");
-        fileWriter.close();
+        Date date = null;
+
+
+        int i = 1;
+        while (i != 0) {
+            date = new Date();
+            System.out.println(date.toString());
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(date.toString());
+            fileWriter.write(" -> created by Version 3");
+            fileWriter.close();
+            TimeUnit.SECONDS.sleep(10);
+
+        }
+
+
     }
 
 }
